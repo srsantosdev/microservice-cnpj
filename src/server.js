8a85da93b2fd8604002cdc4cda49c331d9c1484f
Antonio Raian <raymendesjr2013@gmail.com>
2020-06-11 16:28:09 -0300

    Add Port to log

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const app = require("./app");
 
-app.listen(3004, () => {
+const port = 3004;
+
+app.listen(port, () => {
   console.log("[INFO] Starting...");
   console.log("[INFO] EXECUTING MICROSERVICE CNPJ.");
+  console.log(`[PORT] ${port}`);
 });
